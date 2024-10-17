64cd5d502a204427c4a4aef023439dcb05e9bb0f
Yuriy <kabanpuxluy@gmail.com>
2024-10-17 23:22:50 +0300

    Set correct batch size for KAN

--- kan_fingers_classifier.py
+++ kan_fingers_classifier.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
 import numpy as np
+import time
 
 df1 = pd.read_csv("./ELECTROENCEPHALOGRAM_OF_SENSITIVITY_BODIES/specified_finger.csv",
                   delimiter=";", decimal=".", header=0)
@@ -15,10 +16,8 @@ df2 = pd.read_csv("./ELECTROENCEPHALOGRAM_OF_SENSITIVITY_BODIES/thumb_finger.csv
 df1["target"] = "specified_finger"
 df2["target"] = "thumb_finger"
 
-
 first_10k = df1.iloc[:10000]
 second_10k = df2.iloc[-10000:]
-
 
 df = pd.concat([first_10k, second_10k])
 #Removing duplicate rows
@@ -60,27 +59,31 @@ custom_dataset = {
     'test_label': y_test.view(-1, 1)
 }
 
-model = KAN(width=[16,5,1], grid=5, k=3)
+model = KAN(width=[16,2,1], grid=5, k=3)
+
 
 def mean_squared_error(y_true, y_guess):
     return np.mean((y_true - y_guess) ** 2)
 
 def train_acc():
-    mse = mean_squared_error(custom_dataset['train_label'][:,0].detach().numpy(), model(custom_dataset['train_input'])[:,0].detach().numpy())
-    return 1 / (1 + mse)
+    return torch.mean((torch.round(model(custom_dataset['train_input'])[:,0])
+                       == custom_dataset['train_label'][:,0]).float())
 
 def test_acc():
-    mse = mean_squared_error(custom_dataset['test_label'][:,0].detach().numpy(), model(custom_dataset['test_input'])[:,0].detach().numpy())
-    return 1 / (1 + mse)
+    return torch.mean((torch.round(model(custom_dataset['test_input'])[:,0])
+                       == custom_dataset['test_label'][:,0]).float())
 
-
-results = model.train(custom_dataset, opt="LBFGS", steps=15,
-                      metrics=(train_acc, test_acc), beta=7)
+start_time = time.time()
+results = model.train(custom_dataset, opt="LBFGS", steps=20,
+                      metrics=(train_acc, test_acc), beta=7, save_fig=True)
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time} seconds")
 
 model.prune()
 model.plot(beta=2, scale=1, title='Trained KAN Model')
 
-y_pred = model(x_test)
+y_pred = model(x_test).flatten()
 y_pred = y_pred.detach().numpy()
 y_pred[y_pred < 0] = 0
 y_pred = np.round(y_pred) 
@@ -105,7 +108,6 @@ roc_auc_score = roc_auc_score(y_test, y_pred)
 
 # print('train acc of the formula:', acc(formula1, formula2, custom_dataset['train_input'], custom_dataset['train_label']))
 # print('test acc of the formula:', acc(formula1, formula2, custom_dataset['test_input'], custom_dataset['test_label']))
-
 
 
 
